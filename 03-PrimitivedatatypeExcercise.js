4b3430da21703bf49cfc1ebeaafdaeb508c81336
Prabaharan07 <prabaharan087@gmail.com>
2026-04-26 11:19:04 +0530

    JS Primitive Data Types Exercise

---
+++ 03-PrimitivedatatypeExcercise.js
@@ -0,0 +1,23 @@
+var firstName='Tester'
+console.log(firstName);
+console.log(typeof(firstName));
+
+var companyName='LTM'
+console.log(companyName);
+console.log(typeof(companyName));
+
+var mobileNumber=1234568
+console.log(mobileNumber);
+console.log(typeof(mobileNumber));
+
+var isAutomation=false
+console.log(isAutomation);
+console.log(typeof(isAutomation));
+
+var hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof(hasPlaywright));``
+
+var middlename=null
+console.log(middlename);
+console.log(typeof(middlename));
